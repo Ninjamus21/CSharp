1aab89542fe5a6ef466ea6db20decf2bbc7c0cd7
Nicolai <nicolaiknudsen@outllok.dk>
2022-08-20 00:11:58 +0200

    Stage 2 Not Done

--- 7_Assignment/Dealership.cs
+++ 7_Assignment/Dealership.cs
@@ -32,25 +32,82 @@ namespace Store {
         {
             if (availability > 0)
             {
-                Console.WriteLine(Name + "are in storage and we have " + availability + "left.");
+                Console.WriteLine(Name + " are in storage and we have " + availability + " left.");
             } else {
                 Console.WriteLine("sadly we ran out come back later ;(");
             }
         }
     }
+    class CarDealership
+    {
+        public void Entry()
+        {
+            Console.WriteLine("Hello, Welcome to my humble CarDealership, with over 50% high end cars");
+        }
+        public void ReEntry()
+        {
+            Console.WriteLine("Is there any thing else i can help with?");
+         
+    }
+  
     class program 
     {
         static void Main(string[] args)
             {
-                cars car01 = new cars("omega", 240, "1982", 1000, 10000, 240, "Automatic", 2.7f, 1);
+            #region porsche
+                cars car01 = new cars("Porsche Taycan", 230, "2021", 819900, 0, 326, "Automatic", 5.4f, 1);
+                cars car02 = new cars("Porshe Macan Turbo", 266, "2013", 749900, 98000, 400, "Automatic", 4.8f, 1);
+                cars car03 = new cars("Porsche 911 Targa 4 GTS", 307, "2022", 3453427, 0, 480, "Automatic", 3.5f, 1);
+            #endregion porsche
+            #region Mercedes
+                cars car04 = new cars("Mercedes A180", 190, "2014", 169900, 111800, 109, "Automatic", 11.6f, 1);
+                cars car05 = new cars("Mercedes S560", 250, "2018", 2299900, 11000, 469, "Automatic", 4.6f, 1);
+                cars car06 = new cars("Mercedes E220 d", 240, "2019", 457000, 65000, 194, "Automatic", 7.3f, 1);
+            #endregion Mercedes
+            #region Audi
+                cars car07 = new cars("Audi A3", 203, "2008", 59900, 180000, 125, "Automatic", 9.5f, 2);
+                cars car08 = new cars("Audi SQ5", 250, "2018", 719000, 51000, 354, "Automatic", 5.4f, 1); 
+                cars car09 = new cars("Audi e-tron GT", 245, "2022", 1084900, 1000, 476, "Automatic", 4.1f, 1);
+            #endregion Audi
+            #region BMW
+                cars car10 = new cars("BMW 320d", 230, "2014", 184900, 186000, 184, "Automatic", 7.6f, 1);
+                cars car11 = new cars("BMW X6", 236, "2012", 459500, 114000, 304, "Automatic", 6.5f, 2);
+                cars car12 = new cars("BMW iX", 200, "2022", 819000, 9000, 326, "Automatic", 6.1f, 1);
+            #endregion BMW
+                Console.Write(car11.Name + " TopSpeed is ");
+                Console.WriteLine(car11.TopSpeed);
 
-                car01.BuyCar();
+                Console.Title = "CarDealership";
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WindowHeight = 40;
+/*
+                bool confirmed = false;
+                string Key;
+                do {
+                    Console.Write("Please enter a login key: ");
+                    Key = Console.ReadLine();
+                    Console.WriteLine("You entered, " + Key + " as your login key!");
 
+                    ConsoleKey response;
+                    do
+                    {
+                        Console.Write("Are you sure you want to choose this as your login key? [y/n] ");
+                        response = Console.ReadKey(false).Key;   // true is intercept key (dont show), false is show
+                        if (response != ConsoleKey.Enter)
+                            Console.WriteLine();
+
+                    } while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+                    confirmed = response == ConsoleKey.Y;
+                } while (!confirmed);
+                Console.WriteLine("You chose {0}!", Key);
+                Console.ReadLine();
+*/
                 // Wait for console so it dont quick close
                 Console.ReadKey();
-            }
+        }
     }
 }
-
+}
 
 
